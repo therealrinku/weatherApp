8aec0bc56e329a007402bca6164b42813fdfa38a
therealrinku <backupfb2032@gmail.com>
2021-06-09 16:30:54 +0545

    cities list added

--- client/src/pages/homepage.js
+++ client/src/pages/homepage.js
@@ -1,7 +1,16 @@
+import { Link } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import styles from "../styles/homepage.module.css";
 
 export default function Homepage() {
+  const cities = [
+    { cityName: "Hongkong" },
+    { cityName: "Paris" },
+    { cityName: "London" },
+    { cityName: "California" },
+    { cityName: "Tokyo" },
+  ];
+
   return (
     <>
       <Navbar />
@@ -9,6 +18,13 @@ export default function Homepage() {
         <img src="https://bit.ly/3vbajnj" alt="hero-image" />
         <section>
           <h3>Get Latest Weather Information</h3>
+          {cities.map((city) => {
+            return (
+              <Link key={city.cityName} to={`/weather/${city.cityName}`}>
+                {city.cityName}
+              </Link>
+            );
+          })}
         </section>
       </main>
     </>
