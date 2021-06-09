f2260bde836e9f8759e3926d75ab828e3d103b9b
therealrinku <backupfb2032@gmail.com>
2021-06-09 22:03:44 +0545

    context update + passed to all pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,21 @@
+import { useState } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import AuthPage from "./pages/authpage";
 import Homepage from "./pages/homepage";
 import WeatherDetailPage from "./pages/weatherDetailPage";
+import UserContext from "./userContext";
 
 export default function App() {
+  const [userEmail, setUserEmail] = useState("");
+  const [userAccessToken, setUserAccessToken] = useState("");
+
   return (
     <Router>
-      <Route path="/" exact component={Homepage} />
-      <Route path="/auth" exact component={AuthPage} />
-      <Route path="/weather/:city" exact component={WeatherDetailPage} />
+      <UserContext.Provider value={{ userEmail, setUserEmail, userAccessToken, setUserAccessToken }}>
+        <Route path="/" exact component={Homepage} />
+        <Route path="/auth" exact component={AuthPage} />
+        <Route path="/weather/:city" exact component={WeatherDetailPage} />
+      </UserContext.Provider>
     </Router>
   );
 }
--- client/src/userContext.js
+++ client/src/userContext.js
@@ -2,7 +2,9 @@ import React from "react";
 
 const UserContext = React.createContext({
   userEmail: "",
+  setUserEmail: () => {},
   userAccessToken: "",
+  setUserAccessToken: () => {},
 });
 
 export default UserContext;
