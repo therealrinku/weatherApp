110201fc795755a4839c6fedd6162cf41ea8a7ea
therealrinku <backupfb2032@gmail.com>
2021-06-09 22:31:57 +0545

    login button added to homepage if user is not logged in

--- client/src/pages/homepage.js
+++ client/src/pages/homepage.js
@@ -2,8 +2,13 @@ import { Link } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import styles from "../styles/homepage.module.css";
 import cities from "../cities";
+import { useContext } from "react";
+import UserContext from "../userContext";
+import LoginButton from "../components/LoginButton";
 
 export default function Homepage() {
+  const { userEmail } = useContext(UserContext);
+
   return (
     <>
       <Navbar />
@@ -11,13 +16,17 @@ export default function Homepage() {
         <img src="https://bit.ly/3vbajnj" alt="hero-image" />
         <section>
           <h3>Get Latest Weather Information</h3>
-          {cities.map((city) => {
-            return (
-              <Link key={city.cityName} to={`/weather/${city.cityName}`}>
-                {city.cityName}
-              </Link>
-            );
-          })}
+          {userEmail ? (
+            cities.map((city) => {
+              return (
+                <Link key={city.cityName} to={`/weather/${city.cityName}`}>
+                  {city.cityName}
+                </Link>
+              );
+            })
+          ) : (
+            <LoginButton />
+          )}
         </section>
       </main>
     </>
