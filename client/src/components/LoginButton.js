6813a7fd77695600b99ca3c34743dd53890bbd2b
therealrinku <backupfb2032@gmail.com>
2021-06-09 22:30:01 +0545

    login button component created

---
+++ client/src/components/LoginButton.js
@@ -0,0 +1,12 @@
+import { useHistory } from "react-router-dom";
+import styles from "../styles/loginButton.module.css";
+
+export default function LoginButton() {
+  const history = useHistory();
+
+  return (
+    <button className={styles.loginButton} onClick={() => history.push("/auth")}>
+      Login
+    </button>
+  );
+}
