24401c1b12146084c75aba46060a5f3df19f5d3d
therealrinku <backupfb2032@gmail.com>
2021-06-09 22:58:51 +0545

    icons removed

--- client/src/pages/weatherDetailPage.js
+++ client/src/pages/weatherDetailPage.js
@@ -17,12 +17,13 @@ export default function WeatherDetailPage() {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
-  const imagesForThisWeather = {};
+  //images for this weather type
+  const [imagesForThisWeather, setImagesForThisWeather] = useState([]);
 
   const assetForDifferentWeathers = [
-    { for: "Clouds", mainImageUrl: "https://bit.ly/3w87k0q", icon: <IoIosCloudy /> },
-    { for: "Rain", mainImageUrl: "https://bit.ly/356egPE", icon: <IoRainyOutline /> },
-    { for: "Clear", mainImageUrl: "https://bit.ly/3waBpMN", icon: <IoSunnySharp /> },
+    { for: "Clouds", mainImageUrl: "https://bit.ly/3w87k0q" },
+    { for: "Rain", mainImageUrl: "https://bit.ly/356egPE" },
+    { for: "Clear", mainImageUrl: "https://bit.ly/3waBpMN" },
   ];
 
   useEffect(() => {
@@ -38,9 +39,8 @@ export default function WeatherDetailPage() {
 
         //setting different images based on weather
         assetForDifferentWeathers.forEach((asset) => {
-          if (asset.for === res.data.weather[0]?.main) {
-            imagesForThisWeather.mainImageUrl = asset.mainImageUrl;
-            imagesForThisWeather.icon = asset.icon;
+          if (asset.for == res.data.weather[0]?.main) {
+            setImagesForThisWeather((prev) => [...prev, { mainImageUrl: asset.mainImageUrl }]);
           }
         });
       })
@@ -57,14 +57,11 @@ export default function WeatherDetailPage() {
           <Navbar />
           <main className={styles.weatherDetailPage}>
             <section
-              style={{ backgroundImage: `url(${imagesForThisWeather.mainImageUrl || "https://bit.ly/3gqSH1M"})` }}
+              style={{ backgroundImage: `url(${imagesForThisWeather[0]?.mainImageUrl || "https://bit.ly/3gqSH1M"})` }}
             >
               <h2>{weatherData.main?.temp} farenheit</h2>
               <p style={{ fontWeight: "bold", marginTop: "-10px" }}>{weatherData.name}</p>
-              <span>
-                {imagesForThisWeather.icon || <IoSunnySharp />}
-                {weatherData.weather ? <p>{weatherData.weather[0]?.description}</p> : null}
-              </span>
+              <span>{weatherData.weather ? <p>{weatherData.weather[0]?.description}</p> : null}</span>
             </section>
             <section>
               <p style={{ borderBottom: "solid 1px black" }}>Details</p>
