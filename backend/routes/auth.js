456e32a8b0794a3bb7863dfc8a446c2cc3dc8309
therealrinku <backupfb2032@gmail.com>
2021-06-09 21:19:10 +0545

    creating and sending jwt if login is successfull

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,30 +1,34 @@
 const router = require("express").Router();
 const db = require("../db");
 const bcrypt = require("bcrypt");
+const configs = require("../config");
+const jwt = require("jsonwebtoken");
 
 router.post("/login", (req, res) => {
   db.query(`SELECT email,password FROM users WHERE email='${req.body.email}'`, (err, res1) => {
     if (res1.rows.length > 0) {
       const { email, password } = res1.rows[0];
-      //jwt logic here
 
-      /*
+      //validating password
       bcrypt.compare(req.body.password, password, (err, result) => {
-        if (result) res.send({ profile_image_url, uid, username, bio, email });
-        else res.send("invalid password");
-      });
+        if (result) {
+          //creating jwt if password matched
+          const user = { email };
+          const token = jwt.sign(user, configs.ACCESS_TOKEN_SECRET);
 
-      */
+          res.status(200).send({ email, token });
+        } else res.status(401).send("invalid password");
+      });
     } else {
-      res.send("invalid username");
+      res.status(401).send("invalid username");
     }
   });
 });
 
-//signup user
 router.post("/signup", (req, res) => {
   db.query(`SELECT email FROM users WHERE email='${req.body.email}'`, (err, res1) => {
     if (res1.rows.length <= 0) {
+      //hashing password
       bcrypt.hash(req.body.password, 10, (err1, hashedPassword) => {
         if (!err) {
           db.query(
