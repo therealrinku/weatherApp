d2e315bf9727d1d972631811f05ca68b16a63272
therealrinku <backupfb2032@gmail.com>
2021-06-09 16:23:42 +0545

    font changed and homepage updated

--- client/src/pages/homepage.js
+++ client/src/pages/homepage.js
@@ -1,9 +1,16 @@
 import Navbar from "../components/Navbar";
+import styles from "../styles/homepage.module.css";
 
 export default function Homepage() {
   return (
-    <div>
+    <>
       <Navbar />
-    </div>
+      <main className={styles.homepage}>
+        <img src="https://bit.ly/3vbajnj" alt="hero-image" />
+        <section>
+          <h3>Get Latest Weather Information</h3>
+        </section>
+      </main>
+    </>
   );
 }
