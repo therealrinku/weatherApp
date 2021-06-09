f432f745182c4aeabd38f6599687d01e101632e8
therealrinku <backupfb2032@gmail.com>
2021-06-09 18:20:48 +0545

    getting weather info setup done

--- backend/routes/weather.js
+++ backend/routes/weather.js
@@ -4,17 +4,13 @@ const router = require("express").Router();
 
 //it's defintely not a good idea to keep apiKey here but due to lack of time , i was unable to store it in env
 const apiKey = "d289ddb0716da059849493624687c4d5";
-const apiURL = "api.openweathermap.org/data/2.5/weather?";
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?";
 
 router.get("/:cityName", async (req, res) => {
-  const data = await axios
-    .get(apiURL + `q=${req.params.cityName}&appid=${apiKey}`)
-    .then((dta) => {
-      return dta;
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+  const data = await axios.get(apiURL + `q=${req.params.cityName}&appid=${apiKey}`).then((dta) => {
+    return dta.data;
+  });
+
   res.send(data);
 });
 
