cbba6310a1c00f9729ba8d9294fc09303c9cc28f
therealrinku <backupfb2032@gmail.com>
2021-06-09 21:44:00 +0545

    verifying jwt setup added

--- backend/routes/weather.js
+++ backend/routes/weather.js
@@ -1,10 +1,23 @@
 const { default: axios } = require("axios");
 const router = require("express").Router();
 const configs = require("../config");
+const jwt = require("jsonwebtoken");
+const configs = require("../config");
 
 const apiURL = "https://api.openweathermap.org/data/2.5/weather?";
 
-router.get("/:cityName", async (req, res) => {
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+  if (!token) res.status(401).send("Invalid Token");
+
+  jwt.verify(token, configs.ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) return res.status(403).status("No Access");
+    req.user = user;
+  });
+}
+
+router.get("/:cityName", verifyJWT, async (req, res) => {
   const data = await axios.get(apiURL + `q=${req.params.cityName}&appid=${configs.API_KEY}`).then((dta) => {
     return dta.data;
   });
